d224819867fa8f27df44890f2f9d0d9f9113aa95
Leo "Funa" Funabashi <leo.funa@gmail.com>
2024-09-07 09:51:38 -0300

    test: add some example tests (#1) (#2)

---
+++ test/balance.test.js
@@ -0,0 +1,10 @@
+import { deepStrictEqual } from 'node:assert';
+
+
+;(async () => {
+    {
+        console.log('Test /balance')
+        deepStrictEqual(1, 1);
+        console.log('Passed')
+    }
+})()
