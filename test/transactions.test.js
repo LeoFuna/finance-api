088e8d36b6e016e94b4b36fe88e2be32606d7ee7
LeoFuna <leo.funa@gmail.com>
2024-09-07 07:55:58 -0300

    test: add some example tests

---
+++ test/transactions.test.js
@@ -0,0 +1,10 @@
+import { deepStrictEqual } from 'node:assert';
+
+
+;(async () => {
+    {
+        console.log('Test /transactions')
+        deepStrictEqual(1, 1);
+        console.log('Passed')
+    }
+})()
