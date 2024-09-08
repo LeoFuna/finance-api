60d6942f5c0d0e8c6f76b7c4822cc3d780dfb95f
LeoFuna <leo.funa@gmail.com>
2024-09-07 22:28:31 -0300

    change hello msg

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end('Hello GCP Cloud Run!');
 });
 
 app.get('/transactions', (req, res) => {
